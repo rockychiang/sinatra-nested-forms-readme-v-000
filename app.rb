34e525c4a85f58fe1ec36fa8c4b68326125c4358
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-05 23:45:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,16 +7,15 @@ class App < Sinatra::Base
   end
   
   post '/student' do
-  @student = Student.new(params[:student])
- 
-  params[:student][:courses].each do |details|
-    Course.new(details)
+    @student = Student.new(params[:student])
+   
+    params[:student][:courses].each do |details|
+      Course.new(details)
+    end
+   
+    @courses = Course.all
+   
+    erb :student
   end
- 
-  @courses = Course.all
- 
-  erb :student
-end
-
   
 end
